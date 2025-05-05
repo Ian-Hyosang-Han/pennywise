035e8f6cd6fd993298781fa7888330044021a704
HHan0330 <kkoma330@gmail.com>
2025-05-05 13:04:03 -0700

    fix: remove base path so assets load from root

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pennywise/',
+  base: '/pennywise',
   plugins: [react(), tailwindcss(),],
 })
